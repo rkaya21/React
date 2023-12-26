52958b7b167d5b4a788fdb7a3f78c7627b207c27
Recep Enes Kaya <116019903+rkaya21@users.noreply.github.com>
2023-12-26 04:50:21 +0300

    Update setuptest.js

--- Sistem Analizi ve Tasarımı/Web Uygulaması/src/setupTests.js
+++ Sistem Analizi ve Tasarımı/Web Uygulaması/src/setupTests.js
@@ -1,5 +1,5 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
+// jest-dom, DOM düğümleri üzerinde doğrulama yapmak için özel jest eşleştirmeleri ekler.
+// şunları yapmanıza olanak tanır:
 // expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// daha fazla bilgi için: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
